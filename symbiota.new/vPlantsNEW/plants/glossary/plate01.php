1824e7a07d859479be87d15c1577ef7c59872735
kelueders@gmail.com <kelueders@gmail.com@777f1ae2-0b62-46ad-408f-69f39a835e02>
2014-12-09 22:21:17 +0000

    created plants/glossary/plate01.php

---
+++ symbiota.new/vPlantsNEW/plants/glossary/plate01.php
@@ -0,0 +1,46 @@
+<?php
+//error_reporting(E_ALL);
+include_once($serverRoot."/config/symbini.php");
+header("Content-Type: text/html; charset=".$charset);
+?>
+<html>
+<head>
+	<title><?php echo $defaultTitle?> Home</title>
+	<link href="<?php echo $serverRoot?>/css/base.css" type="text/css" rel="stylesheet" />
+	<link href="<?php echo $serverRoot?>/css/main.css" type="text/css" rel="stylesheet" />
+	<meta name='keywords' content='' />
+	<script type="text/javascript">
+		<?php include_once($serverRoot.'/config/googleanalytics.php'); ?>
+	</script>
+</head>
+<body>
+	<?php
+	$displayLeftMenu = "true";
+	include($serverRoot."/header.php");
+	?> 
+        <!-- This is inner text! -->
+        <div id="innertext">
+            <h1></h1>
+
+            <div style="margin:20px;">
+            	<p>
+				<a href="index.html">Back to Glossary List</a> | 
+				<a href="plate02.html">Next Plate</a>
+				</p>
+
+				<div class="plate">
+				<img src="<?php echo $clientRoot; ?>/images.vplants/plants/glossary/plate01.jpg" width="700" height="1055" 
+				alt="Line drawings of plant features." 
+				title="Plate 1: Stem and Root Types.">
+
+				<h2>Plate 1: Stem and Root Types</h2>
+				</div>
+            </div>
+        </div>
+
+	<?php
+	include($serverRoot."/footer.php");
+	?> 
+
+</body>
+</html>
