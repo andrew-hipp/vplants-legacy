1ed537cf94918849d11942412ddaaab086ba7867
kelueders@gmail.com <kelueders@gmail.com@777f1ae2-0b62-46ad-408f-69f39a835e02>
2014-12-10 21:21:30 +0000

    created resources/biblio.php, resources/docs.php, resources/docs2.php, resources/docs3.php

---
+++ trunk/symbiota.new/vPlantsNEW/resources/biblio.php
@@ -0,0 +1,46 @@
+<?php
+//error_reporting(E_ALL);
+include_once($serverRoot."/config/symbini.php");
+header("Content-Type: text/html; charset=".$charset);
+?>
+<html>
+<head>
+	<title><?php echo $defaultTitle?>vPlants - General References</title>
+	<link href="<?php echo $serverRoot?>/css/base.css" type="text/css" rel="stylesheet" />
+	<link href="<?php echo $serverRoot?>/css/main.css" type="text/css" rel="stylesheet" />
+	<meta name='keywords' content='' />
+	<script type="text/javascript">
+		<?php include_once($serverRoot.'/config/googleanalytics.php'); ?>
+	</script>
+</head>
+<body>
+	<?php
+	$displayLeftMenu = "true";
+	include($serverRoot."/header.php");
+	?> 
+        <!-- This is inner text! -->
+        <div  id="innertext">
+            <h1>General References</h1>
+
+            <div style="margin:20px;">
+            	<h2></h2>
+				<ul>
+				<li>
+				</li>
+				</ul>
+
+				<p>&nbsp;</p>
+				<p>&nbsp;</p>
+				<p>&nbsp;</p>
+				<p>&nbsp;</p>
+				<p>&nbsp;</p>
+				<p>&nbsp;</p>
+            </div>
+        </div>
+
+	<?php
+	include($serverRoot."/footer.php");
+	?> 
+
+</body>
+</html>
