92d0d6ce38f5d4b28ffa62a1fb2d1fc2910fc05a
kelueders@gmail.com <kelueders@gmail.com@777f1ae2-0b62-46ad-408f-69f39a835e02>
2014-12-10 21:21:30 +0000

    created resources/biblio.php, resources/docs.php, resources/docs2.php, resources/docs3.php

---
+++ symbiota.new/vPlantsNEW/resources/docs.php
@@ -0,0 +1,59 @@
+<?php
+//error_reporting(E_ALL);
+include_once($serverRoot."/config/symbini.php");
+header("Content-Type: text/html; charset=".$charset);
+?>
+<html>
+<head>
+	<title><?php echo $defaultTitle?>vPlants - vPlants Documents</title>
+	<link href="<?php echo $serverRoot?>/css/base.css" type="text/css" rel="stylesheet" />
+	<link href="<?php echo $serverRoot?>/css/main.css" type="text/css" rel="stylesheet" />
+	<meta name='keywords' content='' />
+	<script type="text/javascript">
+		<?php include_once($serverRoot.'/config/googleanalytics.php'); ?>
+	</script>
+</head>
+<body>
+	<?php
+	$displayLeftMenu = "true";
+	include($serverRoot."/header.php");
+	?> 
+        <!-- This is inner text! -->
+        <div  id="innertext">
+            <h1>vPlants Documents</h1>
+
+            <div style="margin:20px;">
+            	<p>Links to web pages and documents. These are reference documents used by the vPlants partners, but may also be of use to the public.</p>
+				<h2></h2>
+				<ul>
+				<li>
+				</li>
+				</ul>
+
+				<p>&nbsp;</p>
+				<p>&nbsp;</p>
+				<p>&nbsp;</p>
+				<p>&nbsp;</p>
+				<p>&nbsp;</p>
+				<p>&nbsp;</p>
+            </div>
+        </div>
+		
+		<div id="content2">
+
+			<div class="box">
+			<h3>...</h3>
+			<ul><li>...</li>
+			</ul>
+			</div>
+
+			<p class="small">Information provided on this page applies to the Chicago Region and may not be relevant or complete for other regions.</p><p class="small noprint"><a href="/disclaimer.html" title="Read Disclaimer.">Disclaimer</a></p>
+
+		</div><!-- end of #content2 -->
+
+	<?php
+	include($serverRoot."/footer.php");
+	?> 
+
+</body>
+</html>
