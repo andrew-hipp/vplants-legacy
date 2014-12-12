7ca0546b9b5147e747d5892668b727bd87cb2fbc
kelueders@gmail.com <kelueders@gmail.com@777f1ae2-0b62-46ad-408f-69f39a835e02>
2014-12-12 17:47:12 +0000

    created fungi/guide/index.php, inocybe.php, limacella.php, melanophyllum.php

---
+++ trunk/symbiota.new/vPlantsNEW/fungi/guide/melanophyllum.php
@@ -0,0 +1,55 @@
+<?php
+//error_reporting(E_ALL);
+include_once($serverRoot."/config/symbini.php");
+header("Content-Type: text/html; charset=".$charset);
+?>
+<html>
+<head>
+	<title><?php echo $defaultTitle?>vPlants - Guide to Melanophyllum</title>
+	<link href="<?php echo $serverRoot?>/css/base.css" type="text/css" rel="stylesheet" />
+	<link href="<?php echo $serverRoot?>/css/main.css" type="text/css" rel="stylesheet" />
+	<meta name='keywords' content='' />
+	<script type="text/javascript">
+		<?php include_once($serverRoot.'/config/googleanalytics.php'); ?>
+	</script>
+</head>
+<body>
+	<?php
+	$displayLeftMenu = "true";
+	include($serverRoot."/header.php");
+	?> 
+        <!-- This is inner text! -->
+        <div  id="innertext">
+            <h1>Guide to Melanophyllum</h1>
+
+            <div style="margin:20px;">
+            	<p class="small">This guide applies to the Chicago Region and is not complete for other regions. <span class="noprint"><a href="/disclaimer.html" title="Read Disclaimer.">Disclaimer</a></span></p>
+
+
+				<p>One rare species in the Chicago Region: <a href="http://www.nahuby.sk/obrazok_detail.php?obrazok_id=983">Melanophyllum echinatum</a>.</p>
+
+
+				<p>&nbsp;</p>
+
+
+				<p>&nbsp;</p>
+
+				<p>&nbsp;</p>
+
+				<p>&nbsp;</p>
+
+
+				<p>&nbsp;</p>
+
+				<p>&nbsp;</p>
+
+				<p>&nbsp;</p>
+            </div>
+        </div>
+
+	<?php
+	include($serverRoot."/footer.php");
+	?> 
+
+</body>
+</html>
