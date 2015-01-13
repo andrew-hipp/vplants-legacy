b9594a7cced73b52eb2c959642ce221bf3ef267f
kelueders@gmail.com <kelueders@gmail.com@777f1ae2-0b62-46ad-408f-69f39a835e02>
2015-01-13 22:58:56 +0000

    attempted to add left side menu

--- trunk/symbiota.new/vPlantsNEW/about/index.php
+++ trunk/symbiota.new/vPlantsNEW/about/index.php
@@ -17,7 +17,40 @@ header("Content-Type: text/html; charset=".$charset);
 	<?php
 	$displayLeftMenu = "true";
 	include($serverRoot."/header.php");
-	?> 
+	?>
+		<!-- Left menu begins -->
+		<div class="menu">
+			<div class="menuheader">
+				About Us
+			</div>
+			<div class="menuitem">
+				<a href="<?php echo $clientRoot; ?>/about/vplants.php">
+					vPlants Project
+				</a>
+			</div>
+			<div class="menuitem">
+				<a href="<?php echo $clientRoot; ?>/about/chicago.php">
+					Chicago Region
+				</a>
+			</div>
+			<div class="menuitem">
+				<a href="<?php echo $clientRoot; ?>/about/partnership.php">
+					vPlants Partnership
+				</a>
+			</div>
+			<div class="menuitem">
+				<a href="<?php echo $clientRoot; ?>/about/contact.php">
+					Contact Us
+				</a>
+			</div>
+			<div class="menuitem">
+				<a href="<?php echo $clientRoot; ?>/about/credits.php">
+					Credits
+				</a>
+			</div>
+		</div>
+		<!-- Left menu ends -->
+		
         <!-- This is inner text! -->
         <div  id="innertext">
             <h1>About Us</h1>
