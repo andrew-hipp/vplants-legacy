3402c3d645a8bcc64806fd64291bab45a1f50205
kelueders@gmail.com <kelueders@gmail.com@777f1ae2-0b62-46ad-408f-69f39a835e02>
2015-01-14 21:16:34 +0000

    combined content from biblio pages

--- symbiota.new/vPlantsNEW/resources/biblio.php
+++ symbiota.new/vPlantsNEW/resources/biblio.php
@@ -5,7 +5,7 @@ header("Content-Type: text/html; charset=".$charset);
 ?>
 <html>
 <head>
-	<title><?php echo $defaultTitle?>vPlants - General References</title>
+	<title><?php echo $defaultTitle?>vPlants - References</title>
 	<link href="<?php echo $serverRoot?>/css/base.css" type="text/css" rel="stylesheet" />
 	<link href="<?php echo $serverRoot?>/css/main.css" type="text/css" rel="stylesheet" />
 	<meta name='keywords' content='' />
@@ -20,21 +20,34 @@ header("Content-Type: text/html; charset=".$charset);
 	?> 
         <!-- This is inner text! -->
         <div  id="innertext">
-            <h1>General References</h1>
+            <h1>References Commonly Used for vPlants</h1>
 
             <div style="margin:20px;">
-            	<h2></h2>
-				<ul>
-				<li>
-				</li>
-				</ul>
+				<p>
+				This is a partial list of published bibliographic references that are commonly used and cited on the vPlants website.  Often, much of the specific information on the Species Description pages has been written with the aid of these sources.  Less commonly used, or other specific sources, are cited on the individual Species Description pages.
+				</p>
 
-				<p>&nbsp;</p>
-				<p>&nbsp;</p>
-				<p>&nbsp;</p>
-				<p>&nbsp;</p>
-				<p>&nbsp;</p>
-				<p>&nbsp;</p>
+				<dl>
+
+				<dt>Deam, C. C. 1940.</dt>
+				<dd><i>Flora of Indiana.</i> Indianapolis: Department of Conservation, Division of Forestry.</dd>
+
+				<dt>Fernald, M. L. 1950.</dt>
+				<dd><i>Gray's manual of botany: A handbook of the flowering plants and ferns of the central and northeastern United States and adjacent Canada.</i> 8th ed. New York: American Book Company.</dd>
+
+				<dt>Gleason, H. A. and A. Cronquist. 1991.</dt>
+				<dd><i>Manual of vascular plants of northeastern United States and adjacent Canada.</i> 2nd ed. New York: The New York Botanical Garden.</dd>
+
+				<dt>Mohlenbrock, R. H. 1986.</dt>
+				<dd><i>Guide to the vascular flora of Illinois.</i> revised and enlarged ed. Carbondale, IL: Southern Illinois University Press.</dd>
+
+				<dt>Swink, F. and G. Wilhelm. 1994.</dt>
+				<dd><i>Plants of the Chicago region.</i> 4th ed. Indianapolis: Indiana Academy of Science.</dd>
+
+				<dt>Voss, E. G. 1972 - 1996.</dt>
+				<dd><i>Michigan flora: A guide to the identification and occurrence of the native and naturalized seed-plants of the state.</i> 3 vols. Bloomfield Hills, MI: Cranbrook Institute of Science.</dd>
+
+				</dl>
             </div>
         </div>
 
