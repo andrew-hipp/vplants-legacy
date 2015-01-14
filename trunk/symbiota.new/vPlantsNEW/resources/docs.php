54bdca7d0439e1bac09072b04eed72437c359365
kelueders@gmail.com <kelueders@gmail.com@777f1ae2-0b62-46ad-408f-69f39a835e02>
2015-01-14 21:03:01 +0000

    combined all content from documents pages onto one

--- trunk/symbiota.new/vPlantsNEW/resources/docs.php
+++ trunk/symbiota.new/vPlantsNEW/resources/docs.php
@@ -20,31 +20,56 @@ header("Content-Type: text/html; charset=".$charset);
 	?> 
         <!-- This is inner text! -->
         <div  id="innertext">
-            <h1>vPlants Documents</h1>
+            <h1>Plant Documents</h1>
 
             <div style="margin:20px;">
-            	<p>Links to web pages and documents. These are reference documents used by the vPlants partners, but may also be of use to the public.</p>
-				<h2></h2>
-				<ul>
-				<li>
-				</li>
-				</ul>
+            	<p>Links to web pages for each document. These are reference documents used by the vPlants partners, but may also be of use to the public.</p>
 
-				<p>&nbsp;</p>
-				<p>&nbsp;</p>
-				<p>&nbsp;</p>
-				<p>&nbsp;</p>
-				<p>&nbsp;</p>
-				<p>&nbsp;</p>
+				<h2>Web page with download:</h2>
+
+				<ol>
+				<li>
+				<h3><a href="plant_checklist.html" 
+				title="Scientific Name Checklist.">Taxon Checklist</a></h3>
+				<p>
+				 vPlants Scientific Name Checklist of all plant species, subspecies and varieties included in vPlants.
+				</p>
+				</li><li>
+				<h3><a href="plant_concern.html" 
+				title="Chicago Region Plants of Concern.">Plants of Concern</a></h3>
+				<p>
+				 All Chicago Region vascular plants currently listed as endangered, threatened, rare, etc.
+				</p>
+				</li><li>
+				<h3><a href="plant_invasive.html" 
+				title="Chicago Region Invasive Plants.">Invasive Plants</a></h3>
+				<p>
+				 All non-native (alien) vascular plants that are considered invasive in the Chicago Region.
+				</p>
+				</li><li>
+				<h3><a href="plant_terms.html" 
+				title="vPlants Accepted Plant Terms.">Accepted Plant Terms</a></h3>
+				<p>
+				 A list of the plant terms that are acceptable for use in the vPlants species descriptions.
+				</p>
+				</li>
+				</ol>
+
+				<!-- 
+				<h2>Other Documents for Plants</h2>
+
+				<p>...</p>
+				-->
             </div>
         </div>
 		
 		<div id="content2">
 
-			<div class="box">
-			<h3>...</h3>
-			<ul><li>...</li>
-			</ul>
+			<div class="box external">
+				<h3>Related Web Sites</h3>
+				<ul><li><a href="http://geo.usace.army.mil/wetland_plants/index.html" 
+				title="External link.">Entry page for the National Wetland Plant List</a></li>
+				</ul>
 			</div>
 
 			<p class="small">Information provided on this page applies to the Chicago Region and may not be relevant or complete for other regions.</p><p class="small noprint"><a href="/disclaimer.html" title="Read Disclaimer.">Disclaimer</a></p>
