afc1f57d949a929a92867ca7c45aed83287f6416
kelueders@gmail.com <kelueders@gmail.com@777f1ae2-0b62-46ad-408f-69f39a835e02>
2015-01-15 19:52:21 +0000

    fixed link in all

--- trunk/symbiota.new/vPlantsNEW/about/index.php
+++ trunk/symbiota.new/vPlantsNEW/about/index.php
@@ -93,7 +93,7 @@ header("Content-Type: text/html; charset=".$charset);
 			</li></ul>
 			</div>
 
-			<p class="small">Information provided on this page applies to the Chicago Region and may not be relevant or complete for other regions.</p><p class="small noprint"><a href="/disclaimer.html" title="Read Disclaimer.">Disclaimer</a></p>
+			<p class="small">Information provided on this page applies to the Chicago Region and may not be relevant or complete for other regions.</p><p class="small noprint"><a href="/disclaimer.php" title="Read Disclaimer.">Disclaimer</a></p>
 
 		</div><!-- end of #content2 -->
 
