5acf243dffd4d82c360a9cd9dc60d613005cb529
kelueders@gmail.com <kelueders@gmail.com@777f1ae2-0b62-46ad-408f-69f39a835e02>
2015-01-15 19:49:28 +0000

    added right side content and menu

--- symbiota.new/vPlantsNEW/about/contact.php
+++ symbiota.new/vPlantsNEW/about/contact.php
@@ -1,13 +1,13 @@
 <?php
 //error_reporting(E_ALL);
-include_once($serverRoot."/config/symbini.php");
+include_once('../config/symbini.php');
 header("Content-Type: text/html; charset=".$charset);
 ?>
 <html>
 <head>
-	<title><?php echo $defaultTitle?> Contact</title>
-	<link href="<?php echo $serverRoot?>/css/base.css" type="text/css" rel="stylesheet" />
-	<link href="<?php echo $serverRoot?>/css/main.css" type="text/css" rel="stylesheet" />
+	<title><?php echo $defaultTitle?> About Us - Contact</title>
+	<link href="../css/base.css" type="text/css" rel="stylesheet" />
+	<link href="../css/main.css" type="text/css" rel="stylesheet" />
 	<meta name='keywords' content='' />
 	<script type="text/javascript">
 		<?php include_once($serverRoot.'/config/googleanalytics.php'); ?>
@@ -18,6 +18,41 @@ header("Content-Type: text/html; charset=".$charset);
 	$displayLeftMenu = "true";
 	include($serverRoot."/header.php");
 	?> 
+		<!-- Left menu begins -->
+		<div class="menu">
+			<div class="menuheader">
+				<a href="<?php echo $clientRoot; ?>/about/index.php">
+					About Us
+				</a>
+			</div>
+			<div class="menuitem">
+				<a href="<?php echo $clientRoot; ?>/about/vplants.php">
+					vPlants Project
+				</a>
+			</div>
+			<div class="menuitem">
+				<a href="<?php echo $clientRoot; ?>/about/chicago.php">
+					Chicago Region
+				</a>
+			</div>
+			<div class="menuitem">
+				<a href="<?php echo $clientRoot; ?>/about/partnership.php">
+					vPlants Partnership
+				</a>
+			</div>
+			<div class="menuitem">
+				<strong>
+					Contact Us
+				</strong>
+			</div>
+			<div class="menuitem">
+				<a href="<?php echo $clientRoot; ?>/about/credits.php">
+					Credits
+				</a>
+			</div>
+		</div>
+		<!-- Left menu ends -->
+		
         <!-- This is inner text! -->
         <div  id="innertext">
             <h1>Contact Us</h1>
@@ -38,6 +73,19 @@ header("Content-Type: text/html; charset=".$charset);
 				<p>&nbsp;</p>
             </div>
         </div>
+		
+		<!-- start of side content -->
+		<div id="content2">
+			<!-- any image width should be 250 pixels -->
+
+			<div class="box">
+			<h3>vPlants is growing</h3>
+			<p ><img src="../feature/250_prairie.jpg" width="210" height="291" alt="Prairie near Chicago."></p>
+			</div>
+
+			<p class="small">Information provided on this page applies to the Chicago Region and may not be relevant or complete for other regions.</p><p class="small noprint"><a href="/disclaimer.html" title="Read Disclaimer.">Disclaimer</a></p>
+
+		</div><!-- end of #content2 -->
 
 	<?php
 	include($serverRoot."/footer.php");
