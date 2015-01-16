c191ab7df8715c6751f45db14873ca89b640020a
kelueders@gmail.com <kelueders@gmail.com@777f1ae2-0b62-46ad-408f-69f39a835e02>
2015-01-16 19:37:46 +0000

    fixed links

--- trunk/symbiota.new/vPlantsNEW/resources/regionherbaria.php
+++ trunk/symbiota.new/vPlantsNEW/resources/regionherbaria.php
@@ -91,7 +91,7 @@ header("Content-Type: text/html; charset=".$charset);
 			</ul>
 			</div>
 
-			<p class="small">Information provided on this page applies to the Chicago Region and may not be relevant or complete for other regions.</p><p class="small noprint"><a href="/disclaimer.php" title="Read Disclaimer.">Disclaimer</a></p>
+			<p class="small">Information provided on this page applies to the Chicago Region and may not be relevant or complete for other regions.</p><p class="small noprint"><a href="../disclaimer.php" title="Read Disclaimer.">Disclaimer</a></p>
 
 		</div><!-- end of #content2 -->
 
