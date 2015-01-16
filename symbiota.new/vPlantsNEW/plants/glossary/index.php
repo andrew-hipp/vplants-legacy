890346b67e3ec9cae1b4725849d084eaae52f3d8
kelueders@gmail.com <kelueders@gmail.com@777f1ae2-0b62-46ad-408f-69f39a835e02>
2015-01-16 20:31:49 +0000

    added left menu

--- symbiota.new/vPlantsNEW/plants/glossary/index.php
+++ symbiota.new/vPlantsNEW/plants/glossary/index.php
@@ -18,6 +18,101 @@ header("Content-Type: text/html; charset=".$charset);
 	$displayLeftMenu = "true";
 	include($serverRoot."/header.php");
 	?> 
+		<!-- Left menu begins -->
+		<div class="menu">
+			<div class="menuheader">
+				<a href="<?php echo $clientRoot; ?>/plants/index.php">About Plants</a>
+			</div>
+			<div class="menuitem">
+				<a href="<?php echo $clientRoot; ?>/plants/chicagoplants.php">Chicago Plants</a>
+			</div>
+			<div class="menuitem">
+				<a href="<?php echo $clientRoot; ?>/plants/guide/index.php">Guide</a>
+			</div>
+			<div class="menuitem">
+				<strong>Glossary</strong>
+			</div>
+				<div class="submenuitem">
+					Family Glossaries
+				</div>
+				<div class="submenuitem">
+					<a href="<?php echo $clientRoot; ?>/plants/glossary/asteraceae.php">Asteraceae</a>
+				</div>
+				<div class="submenuitem">
+					<a href="<?php echo $clientRoot; ?>/plants/glossary/cyperaceae.php">Cyperaceae</a>
+				</div>
+				<div class="submenuitem">
+					<a href="<?php echo $clientRoot; ?>/plants/glossary/poaceae.php">Poaceae</a>
+				</div>
+				<div class="submenuitem">
+					Plates
+				</div>
+				<div class="submenuitem">
+					<a href="<?php echo $clientRoot; ?>/plants/glossary/plate01.php">Stem and Root Types</a>
+				</div>
+				<div class="submenuitem">
+					<a href="<?php echo $clientRoot; ?>/plants/glossary/plate02.php">Leaf Composition, Parts, and Types</a>
+				</div>
+				<div class="submenuitem">
+					<a href="<?php echo $clientRoot; ?>/plants/glossary/plate03.php">Leaf Shapes</a>
+				</div>
+				<div class="submenuitem">
+					<a href="<?php echo $clientRoot; ?>/plants/glossary/plate04.php">Leaf Margins</a>
+				</div>
+				<div class="submenuitem">
+					<a href="<?php echo $clientRoot; ?>/plants/glossary/plate05.php">Leaf Apices, Venation, and Bases</a>
+				</div>
+				<div class="submenuitem">
+					<a href="<?php echo $clientRoot; ?>/plants/glossary/plate06.php">Surface Features</a>
+				</div>
+				<div class="submenuitem">
+					<a href="<?php echo $clientRoot; ?>/plants/glossary/plate07.php">Stem and Leaf Parts, and Variations</a>
+				</div>
+				<div class="submenuitem">
+					<a href="<?php echo $clientRoot; ?>/plants/glossary/plate08.php">Inflorescence Types</a>
+				</div>
+				<div class="submenuitem">
+					<a href="<?php echo $clientRoot; ?>/plants/glossary/plate09.php">Floral Morphology</a>
+				</div>
+				<div class="submenuitem">
+					<a href="<?php echo $clientRoot; ?>/plants/glossary/plate10.php">Corolla Types</a>
+				</div>
+				<div class="submenuitem">
+					<a href="<?php echo $clientRoot; ?>/plants/glossary/plate11.php">Fruit Types</a>
+				</div>
+				<div class="submenuitem">
+					<a href="<?php echo $clientRoot; ?>/plants/glossary/plate12.php">Sedges, Grasses, and Composites</a>
+				</div>
+				<div class="submenuitem">
+					<a href="<?php echo $clientRoot; ?>/plants/glossary/plate_all.php">All</a>
+				</div>
+			<div class="menuitem">
+				<a href="<?php echo $clientRoot; ?>/plants/biology.php">Biology</a>
+			</div>
+			<div class="menuitem">
+				<a href="<?php echo $clientRoot; ?>/plants/biology.php">Diversity</a>
+			</div>
+			<div class="menuitem">
+				<a href="<?php echo $clientRoot; ?>/plants/habitats.php">Habitats</a>
+			</div>
+			<div class="menuitem">
+				<a href="<?php echo $clientRoot; ?>/plants/origin.php">Origin</a>
+			</div>
+			<div class="menuitem">
+				<a href="<?php echo $clientRoot; ?>/plants/distribution.php">Distribution</a>
+			</div>
+			<div class="menuitem">
+				<a href="<?php echo $clientRoot; ?>/plants/names.php">Naming</a>
+			</div>
+			<div class="menuitem">
+				<a href="<?php echo $clientRoot; ?>/plants/collections.php">Collections</a>
+			</div>
+			<div class="menuitem">
+				<a href="<?php echo $clientRoot; ?>/plants/concern.php">Special Concern</a>
+			</div>
+		</div>
+		<!-- Left menu ends -->
+	
         <!-- This is inner text! -->
         <div  id="innertext">
 			<p>
--- symbiota.new/vPlantsNEW/plants/glossary/plate01.php
+++ symbiota.new/vPlantsNEW/plants/glossary/plate01.php
@@ -5,7 +5,7 @@ header("Content-Type: text/html; charset=".$charset);
 ?>
 <html>
 <head>
-	<title><?php echo $defaultTitle?> Home</title>
+	<title><?php echo $defaultTitle?> vPlants - Plate 1 - Stem and Root Types</title>
 	<link href="../../css/base.css" type="text/css" rel="stylesheet" />
 	<link href="../../css/main.css" type="text/css" rel="stylesheet" />
 	<meta name='keywords' content='' />
@@ -18,6 +18,101 @@ header("Content-Type: text/html; charset=".$charset);
 	$displayLeftMenu = "true";
 	include($serverRoot."/header.php");
 	?> 
+		<!-- Left menu begins -->
+		<div class="menu">
+			<div class="menuheader">
+				<a href="<?php echo $clientRoot; ?>/plants/index.php">About Plants</a>
+			</div>
+			<div class="menuitem">
+				<a href="<?php echo $clientRoot; ?>/plants/chicagoplants.php">Chicago Plants</a>
+			</div>
+			<div class="menuitem">
+				<a href="<?php echo $clientRoot; ?>/plants/guide/index.php">Guide</a>
+			</div>
+			<div class="menuitem">
+				<a href="<?php echo $clientRoot; ?>/plants/glossary/index.php">Glossary</a>
+			</div>
+				<div class="submenuitem">
+					Family Glossaries
+				</div>
+				<div class="submenuitem">
+					<a href="<?php echo $clientRoot; ?>/plants/glossary/asteraceae.php">Asteraceae</a>
+				</div>
+				<div class="submenuitem">
+					<a href="<?php echo $clientRoot; ?>/plants/glossary/cyperaceae.php">Cyperaceae</a>
+				</div>
+				<div class="submenuitem">
+					<a href="<?php echo $clientRoot; ?>/plants/glossary/poaceae.php">Poaceae</a>
+				</div>
+				<div class="submenuitem">
+					Plates
+				</div>
+				<div class="submenuitem">
+					<strong>Stem and Root Types</strong>
+				</div>
+				<div class="submenuitem">
+					<a href="<?php echo $clientRoot; ?>/plants/glossary/plate02.php">Leaf Composition, Parts, and Types</a>
+				</div>
+				<div class="submenuitem">
+					<a href="<?php echo $clientRoot; ?>/plants/glossary/plate03.php">Leaf Shapes</a>
+				</div>
+				<div class="submenuitem">
+					<a href="<?php echo $clientRoot; ?>/plants/glossary/plate04.php">Leaf Margins</a>
+				</div>
+				<div class="submenuitem">
+					<a href="<?php echo $clientRoot; ?>/plants/glossary/plate05.php">Leaf Apices, Venation, and Bases</a>
+				</div>
+				<div class="submenuitem">
+					<a href="<?php echo $clientRoot; ?>/plants/glossary/plate06.php">Surface Features</a>
+				</div>
+				<div class="submenuitem">
+					<a href="<?php echo $clientRoot; ?>/plants/glossary/plate07.php">Stem and Leaf Parts, and Variations</a>
+				</div>
+				<div class="submenuitem">
+					<a href="<?php echo $clientRoot; ?>/plants/glossary/plate08.php">Inflorescence Types</a>
+				</div>
+				<div class="submenuitem">
+					<a href="<?php echo $clientRoot; ?>/plants/glossary/plate09.php">Floral Morphology</a>
+				</div>
+				<div class="submenuitem">
+					<a href="<?php echo $clientRoot; ?>/plants/glossary/plate10.php">Corolla Types</a>
+				</div>
+				<div class="submenuitem">
+					<a href="<?php echo $clientRoot; ?>/plants/glossary/plate11.php">Fruit Types</a>
+				</div>
+				<div class="submenuitem">
+					<a href="<?php echo $clientRoot; ?>/plants/glossary/plate12.php">Sedges, Grasses, and Composites</a>
+				</div>
+				<div class="submenuitem">
+					<a href="<?php echo $clientRoot; ?>/plants/glossary/plate_all.php">All</a>
+				</div>
+			<div class="menuitem">
+				<a href="<?php echo $clientRoot; ?>/plants/biology.php">Biology</a>
+			</div>
+			<div class="menuitem">
+				<a href="<?php echo $clientRoot; ?>/plants/biology.php">Diversity</a>
+			</div>
+			<div class="menuitem">
+				<a href="<?php echo $clientRoot; ?>/plants/habitats.php">Habitats</a>
+			</div>
+			<div class="menuitem">
+				<a href="<?php echo $clientRoot; ?>/plants/origin.php">Origin</a>
+			</div>
+			<div class="menuitem">
+				<a href="<?php echo $clientRoot; ?>/plants/distribution.php">Distribution</a>
+			</div>
+			<div class="menuitem">
+				<a href="<?php echo $clientRoot; ?>/plants/names.php">Naming</a>
+			</div>
+			<div class="menuitem">
+				<a href="<?php echo $clientRoot; ?>/plants/collections.php">Collections</a>
+			</div>
+			<div class="menuitem">
+				<a href="<?php echo $clientRoot; ?>/plants/concern.php">Special Concern</a>
+			</div>
+		</div>
+		<!-- Left menu ends -->
+		
         <!-- This is inner text! -->
         <div id="innertext">
             <h1></h1>
@@ -29,7 +124,7 @@ header("Content-Type: text/html; charset=".$charset);
 				</p>
 
 				<div class="plate">
-				<img src="<?php echo $clientRoot; ?>/images/vplants/plants/glossary/plate01.jpg" width="700" height="1055" 
+				<img src="<?php echo $clientRoot; ?>/images/vplants/plants/glossary/plate01.jpg" width="525" height="791" 
 				alt="Line drawings of plant features." 
 				title="Plate 1: Stem and Root Types.">
 
