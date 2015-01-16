06ec8ebf8c74d0c6d9d140e64b9de02b85990550
kelueders@gmail.com <kelueders@gmail.com@777f1ae2-0b62-46ad-408f-69f39a835e02>
2015-01-16 17:13:09 +0000

    added list of different habitats

--- trunk/symbiota.new/vPlantsNEW/plants/habitats.php
+++ trunk/symbiota.new/vPlantsNEW/plants/habitats.php
@@ -5,7 +5,7 @@ header("Content-Type: text/html; charset=".$charset);
 ?>
 <html>
 <head>
-	<title><?php echo $defaultTitle?>vPlants - Topics - Habitats</title>
+	<title><?php echo $defaultTitle?>vPlants - About Plants - Habitats</title>
 	<link href="../css/base.css" type="text/css" rel="stylesheet" />
 	<link href="../css/main.css" type="text/css" rel="stylesheet" />
 	<meta name='keywords' content='' />
@@ -27,6 +27,20 @@ header("Content-Type: text/html; charset=".$charset);
 				<!-- Chicago Wilderness definitions -->
 
 				<p>Most habitats are classified by the plants that grow there. Different plants require varying conditions of air and soil moisture, amount of sunlight, temperature range, and soil type. These environmental or abiotic (non-living) factors determine which plants grow and survive in a particular place. The plants, in turn, provide the living structure of the habitat, whether it is hardwood forest, oak savanna, tall-grass prairie, or sedge meadow. The major plants of a habitat modify the environment.  For example, woodland trees provide shade and may raise soil moisture, allowing other plants to grow there. The entire plant community supports the diversity of other organisms, such as animals, fungi, and micro-organisms, within that community.  In short, plants define the community.</p>
+				
+				<h2>Plant Habitats</h2>
+				
+				<div class="indexheading"><a href="<?php echo $clientRoot; ?>/plants/habitats2.php">Woodlands</a></div>
+				<div class="indexdescription"><p>Add description here...<a href="<?php echo $clientRoot; ?>/plants/habitats2.php">Learn more</a></p></div>
+				
+				<div class="indexheading"><a href="<?php echo $clientRoot; ?>/plants/habitats3.php">Grasslands</a></div>
+				<div class="indexdescription"><p>Add description here...<a href="<?php echo $clientRoot; ?>/plants/habitats3.php">Learn more</a></p></div>
+				
+				<div class="indexheading"><a href="<?php echo $clientRoot; ?>/plants/habitats4.php">Wetlands</a></div>
+				<div class="indexdescription"><p>There are many different types of wetlands found in the Chicago Region. Some examples include bogs, fens, marshes, pannes, ponds, and swamps (see list below). Many of our plants grow only in or around wetlands, yet wetlands are at high risk from human impacts such as drainage, pollution, and invasion by alien plants. <a href="<?php echo $clientRoot; ?>/plants/habitats4.php">Learn more</a></p></div>
+				
+				<div class="indexheading"><a href="<?php echo $clientRoot; ?>/plants/habitats5.php">Urban Areas</a></div>
+				<div class="indexdescription"><p>Add description here...<a href="<?php echo $clientRoot; ?>/plants/habitats5.php">Learn more</a></p></div>
             </div>
         </div>
 		
