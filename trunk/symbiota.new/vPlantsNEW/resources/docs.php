f8b505003432c7a7e31f122b811f667295f43848
kelueders@gmail.com <kelueders@gmail.com@777f1ae2-0b62-46ad-408f-69f39a835e02>
2015-01-16 18:16:21 +0000

    fixed links

--- trunk/symbiota.new/vPlantsNEW/resources/docs.php
+++ trunk/symbiota.new/vPlantsNEW/resources/docs.php
@@ -29,25 +29,25 @@ header("Content-Type: text/html; charset=".$charset);
 
 				<ol>
 				<li>
-				<h3><a href="plant_checklist.html" 
+				<h3><a href="plant_checklist.php" 
 				title="Scientific Name Checklist.">Taxon Checklist</a></h3>
 				<p>
 				 vPlants Scientific Name Checklist of all plant species, subspecies and varieties included in vPlants.
 				</p>
 				</li><li>
-				<h3><a href="plant_concern.html" 
+				<h3><a href="plant_concern.php" 
 				title="Chicago Region Plants of Concern.">Plants of Concern</a></h3>
 				<p>
 				 All Chicago Region vascular plants currently listed as endangered, threatened, rare, etc.
 				</p>
 				</li><li>
-				<h3><a href="plant_invasive.html" 
+				<h3><a href="plant_invasive.php" 
 				title="Chicago Region Invasive Plants.">Invasive Plants</a></h3>
 				<p>
 				 All non-native (alien) vascular plants that are considered invasive in the Chicago Region.
 				</p>
 				</li><li>
-				<h3><a href="plant_terms.html" 
+				<h3><a href="plant_terms.php" 
 				title="vPlants Accepted Plant Terms.">Accepted Plant Terms</a></h3>
 				<p>
 				 A list of the plant terms that are acceptable for use in the vPlants species descriptions.
@@ -72,7 +72,7 @@ header("Content-Type: text/html; charset=".$charset);
 				</ul>
 			</div>
 
-			<p class="small">Information provided on this page applies to the Chicago Region and may not be relevant or complete for other regions.</p><p class="small noprint"><a href="/disclaimer.php" title="Read Disclaimer.">Disclaimer</a></p>
+			<p class="small">Information provided on this page applies to the Chicago Region and may not be relevant or complete for other regions.</p><p class="small noprint"><a href="../disclaimer.php" title="Read Disclaimer.">Disclaimer</a></p>
 
 		</div><!-- end of #content2 -->
 
