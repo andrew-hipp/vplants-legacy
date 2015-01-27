441cd3d64931c2ad462674a9f2a41f64d8bcb171
kelueders@gmail.com <kelueders@gmail.com@777f1ae2-0b62-46ad-408f-69f39a835e02>
2015-01-27 02:27:28 +0000

    created leftmenu.php

---
+++ symbiota.new/vPlantsNEW/leftmenu.php
@@ -0,0 +1,45 @@
+<div id="">
+<div id="sitemenu">
+	<a id="menu1" name="menu1"></a>
+	<h5 class="access">Primary Menu <a href="#menu2" title="Skip this menu.">[Skip]</a></h5>
+	<ul class="home">
+		<li>
+			<span class="start" title="You are here: Home page.">Home</span>
+		</li>
+		<li>
+			<a class="about" href="<?php echo $clientRoot; ?>/about/" title="About vPlants and its partners.">About Us</a>
+		</li>
+		<li>
+			<a class="sitemap" href="<?php echo $clientRoot; ?>/sitemap.php" title="Table of contents.">Site Map</a>
+		</li>
+	</ul>
+	<ul class="plant">
+		<li>
+			<a class="start" href="<?php echo $clientRoot; ?>/plants/" title="Plants start page.">Plants</a>
+		</li>
+	</ul>
+	<ul class="fungus">
+		<li>
+			<a class="start" href="<?php echo $clientRoot; ?>/fungi/" title="Fungi start page.">Fungi</a>
+		</li>
+	</ul>
+	<ul class="tools">
+		<li>
+			<a accesskey="5" class="search" href="<?php echo $clientRoot; ?>/search.php" title="Advanced search for plants or fungi.">Search</a>
+		</li>
+		<li>
+			<a class="help" href="<?php echo $clientRoot; ?>/help/" title="Help and user guide.">Help Guide</a>
+		</li>
+		<li>
+			<a class="topics" href="<?php echo $clientRoot; ?>/topics/" title="Collections, habitats, and more.">Topics</a>
+		</li>
+		<li>
+			<a class="resources" href="<?php echo $clientRoot; ?>/resources/" title="Documents, books, and links.">Resources</a>
+		</li>
+		<li>
+			<a class="news" href="<?php echo $clientRoot; ?>/news/" title="New content.">News</a>
+		</li>
+	</ul>
+</div><!-- end of #sitemenu -->
+</div>
+
