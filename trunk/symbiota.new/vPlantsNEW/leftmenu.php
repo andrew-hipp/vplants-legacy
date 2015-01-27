2e93819f285cca7d34fd2ad4b135df39ece2f4d6
kelueders@gmail.com <kelueders@gmail.com@777f1ae2-0b62-46ad-408f-69f39a835e02>
2015-01-27 03:19:30 +0000

    added links to left menu

--- trunk/symbiota.new/vPlantsNEW/leftmenu.php
+++ trunk/symbiota.new/vPlantsNEW/leftmenu.php
@@ -4,40 +4,99 @@
 	<h5 class="access">Primary Menu <a href="#menu2" title="Skip this menu.">[Skip]</a></h5>
 	<ul class="home">
 		<li>
-			<span class="start" title="You are here: Home page.">Home</span>
+			<a class="start" href="<?php echo $clientRoot; ?>/index.php">Home</a>
+		</li>
+	</ul>
+	<ul class="home">
+		<li>
+			<a class="start" href="<?php echo $clientRoot; ?>/about/index.php">About Us</a>
 		</li>
 		<li>
-			<a class="about" href="<?php echo $clientRoot; ?>/about/" title="About vPlants and its partners.">About Us</a>
+			<a class="region" href="<?php echo $clientRoot; ?>/about/chicago.php">Chicago Region</a>
 		</li>
 		<li>
-			<a class="sitemap" href="<?php echo $clientRoot; ?>/sitemap.php" title="Table of contents.">Site Map</a>
+			<a class="partnership" href="<?php echo $clientRoot; ?>/about/partnership.php">vPlants Partnership</a>
+		</li>
+		<li>
+			<a class="contact" href="<?php echo $clientRoot; ?>/about/contact.php">Contact Us</a>
+		</li>
+		<li>
+			<a class="credits" href="<?php echo $clientRoot; ?>/about/credits.php">Credits</a>
 		</li>
 	</ul>
 	<ul class="plant">
 		<li>
-			<a class="start" href="<?php echo $clientRoot; ?>/plants/" title="Plants start page.">Plants</a>
+			<a class="start" href="<?php echo $clientRoot; ?>/plants/index.php">About Plants</a>
+		</li>
+		<li>
+			<a class="chicagoplants" href="<?php echo $clientRoot; ?>/plants/chicagoplants.php">Chicago Plants</a>
+		</li>
+		<li>
+			<a class="guide" href="<?php echo $clientRoot; ?>/plants/guide/index.php">Guide</a>
+		</li>
+		<li>
+			<a class="glossary" href="<?php echo $clientRoot; ?>/plants/glossary/index.php">Glossary</a>
+		</li>
+		<li>
+			<a class="biology" href="<?php echo $clientRoot; ?>/plants/biology.php">Biology</a>
+		</li>
+		<li>
+			<a class="diversity" href="<?php echo $clientRoot; ?>/plants/diversity.php">Diversity</a>
+		</li>
+		<li>
+			<a class="habitats" href="<?php echo $clientRoot; ?>/plants/habitats.php">Habitats</a>
+		</li>
+		<li>
+			<a class="origin" href="<?php echo $clientRoot; ?>/plants/origin.php">Origin</a>
+		</li>
+		<li>
+			<a class="distribution" href="<?php echo $clientRoot; ?>/plants/distribution.php">Distribution</a>
+		</li>
+		<li>
+			<a class="names" href="<?php echo $clientRoot; ?>/plants/names.php">Names</a>
+		</li>
+		<li>
+			<a class="collections" href="<?php echo $clientRoot; ?>/plants/collections.php">Collections</a>
+		</li>
+		<li>
+			<a class="concern" href="<?php echo $clientRoot; ?>/plants/concern.php">Special Concern</a>
 		</li>
 	</ul>
-	<ul class="fungus">
+	<ul class="plant">
 		<li>
-			<a class="start" href="<?php echo $clientRoot; ?>/fungi/" title="Fungi start page.">Fungi</a>
+			<a class="start" href="<?php echo $clientRoot; ?>/search/index.php">Search</a>
 		</li>
 	</ul>
-	<ul class="tools">
+	<ul class="plant">
 		<li>
-			<a accesskey="5" class="search" href="<?php echo $clientRoot; ?>/search.php" title="Advanced search for plants or fungi.">Search</a>
+			<a class="start" href="<?php echo $clientRoot; ?>/imagelib/index.php">Browse Images</a>
+		</li>
+	</ul>
+	<ul class="plant">
+		<li>
+			<a class="start" href="<?php echo $clientRoot; ?>/projects/index.php?">Inventories</a>
+		</li>
+	</ul>
+	<ul class="plant">
+		<li>
+			<a class="start" href="#">Interactive Tools</a>
+		</li>
+	</ul>
+	<ul class="home">
+		<li>
+			<a class="start" href="#">Interactive Tools</a>
 		</li>
 		<li>
-			<a class="help" href="<?php echo $clientRoot; ?>/help/" title="Help and user guide.">Help Guide</a>
+			<a class="regionherbaria" href="<?php echo $clientRoot; ?>/resources/index.php" >Resources</a>
 		</li>
 		<li>
-			<a class="topics" href="<?php echo $clientRoot; ?>/topics/" title="Collections, habitats, and more.">Topics</a>
+			<a class="docs" href="<?php echo $clientRoot; ?>/resources/docs.php" >Documents</a>
 		</li>
 		<li>
-			<a class="resources" href="<?php echo $clientRoot; ?>/resources/" title="Documents, books, and links.">Resources</a>
+			<a class="biblio" href="<?php echo $clientRoot; ?>/resources/biblio.php" >References</a>
 		</li>
 		<li>
-			<a class="news" href="<?php echo $clientRoot; ?>/news/" title="New content.">News</a>
+			<a class="links" href="<?php echo $clientRoot; ?>/resources/links.php" >Links</a>
 		</li>
 	</ul>
 </div><!-- end of #sitemenu -->
