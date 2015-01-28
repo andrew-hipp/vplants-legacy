9848eebb935882cc0b5035741ed2f47bc608c048
kelueders@gmail.com <kelueders@gmail.com@777f1ae2-0b62-46ad-408f-69f39a835e02>
2015-01-28 03:26:13 +0000

    fixed links

--- trunk/symbiota.new/vPlantsNEW/about/chicago.php
+++ trunk/symbiota.new/vPlantsNEW/about/chicago.php
@@ -41,20 +41,13 @@ header("Content-Type: text/html; charset=".$charset);
 						   title="external link.">Chicago Wilderness consortium (external link)</a>.  This unique organization of groups strives to promote, protect, and preserve the rich biota and flora of this same area surrounding the city of Chicago.  One of the standing goals of vPlants is to provide quality data about the plants and fungi that occur in the Chicago Wilderness area at a central location.  
 						 </p>
 						 <p>
-						  The geographic and geologic features of the Chicago Region support a diversity of <a href="plants/diversity.php" title="Plant Diversity.">plants</a>, <a href="fungi/diversity.php" title="Fungus Diversity.">fungi</a>, and animals.  The climate effects of Lake Michigan have a huge impact on the natural plant communities, but historic geologic features and events, such as the glaciers, played an even bigger part.  Just as the glaciers played a critical role in the overall development of the physiographic features of the entire Great Lakes region, their influence on the land surrounding the southern tip of Lake Michigan was profound.  An early predecessor to Lake Michigan, Glacial Lake Chicago (14,000 to 12,400 years ago), had complex marshes and drainage systems associated with it that were comparable in size to the Florida Everglades.  Today, the sands deposited by historic dune complexes at that time (the Glenwood Beach / Dunes) and those from the later and larger Lake Algonquin (almost 12,000 years ago, Calumet Beach / Dunes) are striking features in terms of inland topography and soil type relative to today&#39;s lakeshore and current active beach and dunes.  Similarly, the clayey glacial till deposits that remain in the Tinley and Valparaiso Moraine systems (originally created about 15,000 years ago), which skirt the Chicago Region have shaped the area&#39;s flora and biota.  The Chicago Region meshes different floristic zones from the north, east, and west, and is also part of major bird and butterfly migration routes.  <cite title="Plants of the Chicago region. 4th ed. Indianapolis: Indiana Academy of Science.">Swink and Wilhelm (1994)</cite> put it elegantly when they said &#8220;it would be difficult to circumscribe another area of the North Temperate Zone with such geologic and physiographic diversity&#8221; and &#8220;our native flora reflects this.&#8221;
+						  The geographic and geologic features of the Chicago Region support a diversity of <a href="<?php echo $clientRoot; ?>/plants/diversity.php" title="Plant Diversity.">plants</a>, fungi, and animals.  The climate effects of Lake Michigan have a huge impact on the natural plant communities, but historic geologic features and events, such as the glaciers, played an even bigger part.  Just as the glaciers played a critical role in the overall development of the physiographic features of the entire Great Lakes region, their influence on the land surrounding the southern tip of Lake Michigan was profound.  An early predecessor to Lake Michigan, Glacial Lake Chicago (14,000 to 12,400 years ago), had complex marshes and drainage systems associated with it that were comparable in size to the Florida Everglades.  Today, the sands deposited by historic dune complexes at that time (the Glenwood Beach / Dunes) and those from the later and larger Lake Algonquin (almost 12,000 years ago, Calumet Beach / Dunes) are striking features in terms of inland topography and soil type relative to today&#39;s lakeshore and current active beach and dunes.  Similarly, the clayey glacial till deposits that remain in the Tinley and Valparaiso Moraine systems (originally created about 15,000 years ago), which skirt the Chicago Region have shaped the area&#39;s flora and biota.  The Chicago Region meshes different floristic zones from the north, east, and west, and is also part of major bird and butterfly migration routes.  <cite title="Plants of the Chicago region. 4th ed. Indianapolis: Indiana Academy of Science.">Swink and Wilhelm (1994)</cite> put it elegantly when they said &#8220;it would be difficult to circumscribe another area of the North Temperate Zone with such geologic and physiographic diversity&#8221; and &#8220;our native flora reflects this.&#8221;
 						 </p>
 						 <p>
-						  Learn more about <a href="plants/diversity.php" 
+						  Learn more about <a href="<?php echo $clientRoot; ?>/plants/diversity.php" 
 						   title="Plant Directory.">Chicago Region plants</a>.
 						 </p>
 						 
-						 <p>
-						  Learn more about <a href="fungi/diversity.php" 
-						   title="Fungus Directory.">Chicago Region fungi</a>.
-						 </p>
-						 
-						 <div id="gomenu">Next &#187; <a href="map.php"  
-						  title="See State Map for Chicago Region.">State Map</a></div>
 					</div>
 					</div><!-- end of #content1 -->
 					</div><!-- end of #content1wrap -->
@@ -71,13 +64,13 @@ header("Content-Type: text/html; charset=".$charset);
 						  <p>
 						   <img class="float" src="<?php echo $clientRoot; ?>/images/vplants/img/map_color_box.gif" width="20" height="20" alt="Green Box"> Region covered by vPlants
 						  </p>
-						</div>
-						 
-						<p><a href="<?php echo $clientRoot; ?>/about/map.php" title="See State Map for Chicago Region."><img class="border" src="<?php echo $clientRoot; ?>/images/vplants/img/map_grtlakes_250.jpg" width="250" height="212"
-						  alt="The vPlants Region is located within four states at the south end of Lake Michigan."></a></p>
 						  
-						<p><a href="<?php echo $clientRoot; ?>/about/map_county.php" title="See County Map for Chicago Region."><img class="border" src="<?php echo $clientRoot; ?>/images/vplants/img/map_vplants.gif" width="250" height="212"
-						  alt="Map of the vPlants Chicago Region showing counties included."></a></p>
+						  <p><a href="map.php" title="See State Map for Chicago Region."><img class="border" src="<?php echo $clientRoot; ?>/images/vplants/img/map_grtlakes_250.jpg" width="240" height="212"
+						  alt="The vPlants Region is located within four states at the south end of Lake Michigan."></a><br><a href="map.php">See larger</a></p>
+						  
+						  <p><a href="map_county.php" title="See County Map for Chicago Region."><img class="border" src="<?php echo $clientRoot; ?>/images/vplants/img/map_vplants.gif" width="240" height="212"
+						  alt="Map of the vPlants Chicago Region showing counties included."></a><br><a href="map_county.php">See larger</a></p>
+						</div>
 						 
 						<div class="box">
 						 <h3>Related Web Sites</h3>
