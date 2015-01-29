e1fedb8d6022da9fd165a8bc074ce2861d3915b5
kelueders@gmail.com <kelueders@gmail.com@777f1ae2-0b62-46ad-408f-69f39a835e02>
2015-01-29 04:32:59 +0000

    fixed positioning

--- trunk/symbiota.new/vPlantsNEW/resources/biblio.php
+++ trunk/symbiota.new/vPlantsNEW/resources/biblio.php
@@ -19,37 +19,51 @@ header("Content-Type: text/html; charset=".$charset);
 	include($serverRoot."/header.php");
 	?> 
         <!-- This is inner text! -->
-        <div  id="innertext">
-            <h1>References Commonly Used for vPlants</h1>
+		<!-- start of inner text and right side content -->
+		<div  id="innertext">
+			<div id="bodywrap">
+				<div id="wrapper1"><!-- for navigation and content -->
 
-            <div style="margin:20px;">
-				<p>
-				This is a partial list of published bibliographic references that are commonly used and cited on the vPlants website.  Often, much of the specific information on the Species Description pages has been written with the aid of these sources.  Less commonly used, or other specific sources, are cited on the individual Species Description pages.
-				</p>
+					<!-- PAGE CONTENT STARTS -->
 
-				<dl>
+					<div id="content1wrap"><!--  for content1 only -->
 
-				<dt>Deam, C. C. 1940.</dt>
-				<dd><i>Flora of Indiana.</i> Indianapolis: Department of Conservation, Division of Forestry.</dd>
+					<div id="content1"><!-- start of primary content --><a id="pagecontent" name="pagecontent"></a>
+						<h1>References Commonly Used for vPlants</h1>
 
-				<dt>Fernald, M. L. 1950.</dt>
-				<dd><i>Gray's manual of botany: A handbook of the flowering plants and ferns of the central and northeastern United States and adjacent Canada.</i> 8th ed. New York: American Book Company.</dd>
+						<div style="margin:20px;">
+							<p>
+							This is a partial list of published bibliographic references that are commonly used and cited on the vPlants website.  Often, much of the specific information on the Species Description pages has been written with the aid of these sources.  Less commonly used, or other specific sources, are cited on the individual Species Description pages.
+							</p>
 
-				<dt>Gleason, H. A. and A. Cronquist. 1991.</dt>
-				<dd><i>Manual of vascular plants of northeastern United States and adjacent Canada.</i> 2nd ed. New York: The New York Botanical Garden.</dd>
+							<dl>
 
-				<dt>Mohlenbrock, R. H. 1986.</dt>
-				<dd><i>Guide to the vascular flora of Illinois.</i> revised and enlarged ed. Carbondale, IL: Southern Illinois University Press.</dd>
+							<dt>Deam, C. C. 1940.</dt>
+							<dd><i>Flora of Indiana.</i> Indianapolis: Department of Conservation, Division of Forestry.</dd>
 
-				<dt>Swink, F. and G. Wilhelm. 1994.</dt>
-				<dd><i>Plants of the Chicago region.</i> 4th ed. Indianapolis: Indiana Academy of Science.</dd>
+							<dt>Fernald, M. L. 1950.</dt>
+							<dd><i>Gray's manual of botany: A handbook of the flowering plants and ferns of the central and northeastern United States and adjacent Canada.</i> 8th ed. New York: American Book Company.</dd>
 
-				<dt>Voss, E. G. 1972 - 1996.</dt>
-				<dd><i>Michigan flora: A guide to the identification and occurrence of the native and naturalized seed-plants of the state.</i> 3 vols. Bloomfield Hills, MI: Cranbrook Institute of Science.</dd>
+							<dt>Gleason, H. A. and A. Cronquist. 1991.</dt>
+							<dd><i>Manual of vascular plants of northeastern United States and adjacent Canada.</i> 2nd ed. New York: The New York Botanical Garden.</dd>
 
-				</dl>
-            </div>
-        </div>
+							<dt>Mohlenbrock, R. H. 1986.</dt>
+							<dd><i>Guide to the vascular flora of Illinois.</i> revised and enlarged ed. Carbondale, IL: Southern Illinois University Press.</dd>
+
+							<dt>Swink, F. and G. Wilhelm. 1994.</dt>
+							<dd><i>Plants of the Chicago region.</i> 4th ed. Indianapolis: Indiana Academy of Science.</dd>
+
+							<dt>Voss, E. G. 1972 - 1996.</dt>
+							<dd><i>Michigan flora: A guide to the identification and occurrence of the native and naturalized seed-plants of the state.</i> 3 vols. Bloomfield Hills, MI: Cranbrook Institute of Science.</dd>
+
+							</dl>
+						</div>
+					</div><!-- end of #content1 -->
+					</div><!-- end of #content1wrap -->
+				</div><!-- end of #wrapper1 -->
+			</div><!-- end of #bodywrap -->
+		</div><!-- end of #innertext -->
+        
 
 	<?php
 	include($serverRoot."/footer.php");
