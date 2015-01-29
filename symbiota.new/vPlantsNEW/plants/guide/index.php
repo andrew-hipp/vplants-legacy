a4d98920595b8ee95433df3d01b965912e25aeaa
kelueders@gmail.com <kelueders@gmail.com@777f1ae2-0b62-46ad-408f-69f39a835e02>
2015-01-29 04:02:46 +0000

    fixed positioning

--- symbiota.new/vPlantsNEW/plants/guide/index.php
+++ symbiota.new/vPlantsNEW/plants/guide/index.php
@@ -19,125 +19,138 @@ header("Content-Type: text/html; charset=".$charset);
 	include($serverRoot."/header.php");
 	?> 
         <!-- This is inner text! -->
-        <div  id="innertext">
-            <h1>Guide to Plants of the Chicago Region</h1>
+		<!-- start of inner text and right side content -->
+		<div  id="innertext">
+			<div id="bodywrap">
+				<div id="wrapper1"><!-- for navigation and content -->
 
-            <div style="margin:20px;">
-            	<p class="small">This guide applies to the Chicago Region and is not complete for other regions. <span class="noprint"><a href="/disclaimer.html" title="Read Disclaimer.">Disclaimer</a></span></p>
+					<!-- PAGE CONTENT STARTS -->
+
+					<div id="content1wrap"><!--  for content1 only -->
+
+					<div id="content1"><!-- start of primary content --><a id="pagecontent" name="pagecontent"></a>
+					<h1>Guide to Plants of the Chicago Region</h1>
+
+					<div style="margin:20px;">
+						<p class="small">This guide applies to the Chicago Region and is not complete for other regions. <span class="noprint"><a href="/disclaimer.html" title="Read Disclaimer.">Disclaimer</a></span></p>
+						
+						<p>Use the <a href="/xsql/plants/famlist.xsql" title="Index of families">Family Index</a> and <a href="/xsql/plants/genlist.xsql" title="Index of genera">Genus Index</a> to see alphabetical lists of included plants. At the top of the page you can Search for plants by name. For a complete list of the plant species, subspecies, and varieties represented in the Chicago Region, see the <a href="/resources/plant_checklist.html" title="vPlants document.">Scientific Name Checklist</a>.</p>
+
+						<table class="key" cellpadding="3" cellspacing="0" border="0">
+						<caption>Groups included in vPlants</caption>
+						<tbody>
+
+						<tr class="keydivision">
+						<td>
+						<h2>Vascular Plants</h2>
+						<p>The vPlants site currently provides information on vascular plants, a group named for the special transport tissues (circulatory system of xylem and phloem) they possess. 
+						The vascular plants make up the great majority of the land plants living today. There are two categories of vascular plants: non-flowering and flowering.  Within these categories there are three main groups:
+						</p>
+						</td>
+						</tr><tr >
+
+						<tr class="keychoice">
+						<td >
+						<h3>Pteridophytes (non-flowering spore plants)</h3>
+						<p>
+						The pteridophyte group includes ferns, horsetails, club mosses, and other vascular plants without seeds. 
+						They reproduce by spores formed on the surface of leaves, or in cone-like structures. The spores grow into a short-lived, haploid, gametophyte stage, which produces gametes.  The male and female gametes unite to form embryos, which grow into adult, sporophyte (spore-producing) plants.
+						</p>
+						</td>
+						</tr>
+
+						<tr class="keychoice">
+						<td >
+						<h3>Gymnosperms (non-flowering seed plants)</h3>
+						<p>
+						The most familiar examples of gymnosperms include conifer trees like pines, spruce, fir, and other plants commonly thought of as "evergreen" (though some lose their leaves in winter or in dry seasons, such as larch and ginkgo). They reproduce by seeds, sometimes in cones, but do not have flowers or fruits.
+						</p>
+						</td>
+						</tr>
+
+						<tr class="keychoice">
+						<td >
+						<h3>Angiosperms (flowering seed plants)</h3>
+						<p>
+						The angiosperms include all flowering plants.  All angiosperms produce flowers as their reproductive structures, though not all flowers are showy or even conspicuous. Flowers produce fruits that contain seeds.  Angiosperms are by far more dominant on Earth today than are the gymnosperms and pteridophytes. It is the most diverse plant group and includes organisms from lawn grasses to oak trees.
+						</p>
+						</td>
+						</tr>
+
+						</tbody>
+						</table>
+
+
+
+						<table class="key" cellpadding="3" cellspacing="0" border="0">
+						<caption>Groups not included in vPlants</caption>
+						<tbody>
+
+						<tr class="keydivision">
+						<td colspan="2">
+						<h2>Non-vascular Plants</h2>
+						<p>These plants lack a true vascular system. Many have a life cycle that has two separate stages. In most cases they require wet habitats or rainwater to allow the sperm to swim between individuals in order to fertilize the eggs.
+						</p>
+						</td>
+						</tr><tr >
+
+						<tr  class="keychoice">
+						<td >
+						<h3>Bryophytes</h3>
+						<p>
+						Bryophytes include mosses, liverworts, and hornworts. These are smaller, green plants, which reproduce by spores. Their green, visible, free-living phase of the life cycle is actually the haploid generation, called the gametophyte.  This phase produces male and female gametes, which unite, and consequently develop into a very small structure pertaining to the spore-producing sporophyte phase.  The haploid spores are then released, and grow into a new gametophyte plant.  Bryophytes also often reproduce asexually. 
+						</p>
+						</td>
+						</tr><tr >
+
+						<tr class="keychoice">
+						<td >
+						<h3>Green Algae</h3>
+						<p>
+						The green algae are the simplest of green, photosynthetic plants. Growth forms can be single cells, filaments, branched networks, or small leafy patches. They display many different life cycle types, usually involving spores at some stage.
+						</p>
+						</td>
+						</tr><tr >
+
+						</tbody>
+						</table>
+					</div>
+					</div><!-- end of #content1 -->
+					</div><!-- end of #content1wrap -->
 				
-				<p>Use the <a href="/xsql/plants/famlist.xsql" title="Index of families">Family Index</a> and <a href="/xsql/plants/genlist.xsql" title="Index of genera">Genus Index</a> to see alphabetical lists of included plants. At the top of the page you can Search for plants by name. For a complete list of the plant species, subspecies, and varieties represented in the Chicago Region, see the <a href="/resources/plant_checklist.html" title="vPlants document.">Scientific Name Checklist</a>.</p>
+					<div id="content2"><!-- start of side content -->
+						<!-- any image width should be 250 pixels -->
 
-				<table class="key" cellpadding="3" cellspacing="0" border="0">
-				<caption>Groups included in vPlants</caption>
-				<tbody>
+						<div class="box">
+						<h3>Related Pages</h3>
+						 <ul><li>
+						  <a href="/plants/diversity.html"
+						   title="How many plants.">Chicago plant diversity</a>
+						 </li><li>
+						  <a href="/topics/" 
+						   title="Collections, habitats, and more.">More information on plants</a>
+						 </li><li>
+						  <a href="/resources/plant_checklist.html" 
+						   title="vPlants document.">Scientific Name Checklist</a>
+						 </li><li>
+						  <a href="/resources/links2.html"
+						   title="Links to related web sites">Links for Plants</a>
+						 </li></ul>
+						</div>
+						 
+						<p>Non-flowering vascular plants</p>
+						<img src="<?php echo $clientRoot; ?>/images.vplants/feature/nonflowering.jpg" width="250" height="342" alt="Non-flowering plant examples: fern frond, a pteridophyte; and spruce branch, a gymnosperm.">
+						<hr>
+						<p>Flowering plants </p>
+						<img src="<?php echo $clientRoot; ?>/images.vplants/feature/angios.jpg" width="250" height="322" alt="Flowering plant examples: flowers of cactus and wild rice, both angiosperms.">
+						 
+						 
+						<p class="small">Information provided on this page applies to the Chicago Region and may not be relevant or complete for other regions.</p><p class="small noprint"><a href="/disclaimer.html" title="Read Disclaimer.">Disclaimer</a></p>
 
-				<tr class="keydivision">
-				<td>
-				<h2>Vascular Plants</h2>
-				<p>The vPlants site currently provides information on vascular plants, a group named for the special transport tissues (circulatory system of xylem and phloem) they possess. 
-				The vascular plants make up the great majority of the land plants living today. There are two categories of vascular plants: non-flowering and flowering.  Within these categories there are three main groups:
-				</p>
-				</td>
-				</tr><tr >
-
-				<tr class="keychoice">
-				<td >
-				<h3>Pteridophytes (non-flowering spore plants)</h3>
-				<p>
-				The pteridophyte group includes ferns, horsetails, club mosses, and other vascular plants without seeds. 
-				They reproduce by spores formed on the surface of leaves, or in cone-like structures. The spores grow into a short-lived, haploid, gametophyte stage, which produces gametes.  The male and female gametes unite to form embryos, which grow into adult, sporophyte (spore-producing) plants.
-				</p>
-				</td>
-				</tr>
-
-				<tr class="keychoice">
-				<td >
-				<h3>Gymnosperms (non-flowering seed plants)</h3>
-				<p>
-				The most familiar examples of gymnosperms include conifer trees like pines, spruce, fir, and other plants commonly thought of as "evergreen" (though some lose their leaves in winter or in dry seasons, such as larch and ginkgo). They reproduce by seeds, sometimes in cones, but do not have flowers or fruits.
-				</p>
-				</td>
-				</tr>
-
-				<tr class="keychoice">
-				<td >
-				<h3>Angiosperms (flowering seed plants)</h3>
-				<p>
-				The angiosperms include all flowering plants.  All angiosperms produce flowers as their reproductive structures, though not all flowers are showy or even conspicuous. Flowers produce fruits that contain seeds.  Angiosperms are by far more dominant on Earth today than are the gymnosperms and pteridophytes. It is the most diverse plant group and includes organisms from lawn grasses to oak trees.
-				</p>
-				</td>
-				</tr>
-
-				</tbody>
-				</table>
-
-
-
-				<table class="key" cellpadding="3" cellspacing="0" border="0">
-				<caption>Groups not included in vPlants</caption>
-				<tbody>
-
-				<tr class="keydivision">
-				<td colspan="2">
-				<h2>Non-vascular Plants</h2>
-				<p>These plants lack a true vascular system. Many have a life cycle that has two separate stages. In most cases they require wet habitats or rainwater to allow the sperm to swim between individuals in order to fertilize the eggs.
-				</p>
-				</td>
-				</tr><tr >
-
-				<tr  class="keychoice">
-				<td >
-				<h3>Bryophytes</h3>
-				<p>
-				Bryophytes include mosses, liverworts, and hornworts. These are smaller, green plants, which reproduce by spores. Their green, visible, free-living phase of the life cycle is actually the haploid generation, called the gametophyte.  This phase produces male and female gametes, which unite, and consequently develop into a very small structure pertaining to the spore-producing sporophyte phase.  The haploid spores are then released, and grow into a new gametophyte plant.  Bryophytes also often reproduce asexually. 
-				</p>
-				</td>
-				</tr><tr >
-
-				<tr class="keychoice">
-				<td >
-				<h3>Green Algae</h3>
-				<p>
-				The green algae are the simplest of green, photosynthetic plants. Growth forms can be single cells, filaments, branched networks, or small leafy patches. They display many different life cycle types, usually involving spores at some stage.
-				</p>
-				</td>
-				</tr><tr >
-
-				</tbody>
-				</table>
-            </div>
-        </div>
-		
-		<div id="content2"><!-- start of side content -->
-			<!-- any image width should be 250 pixels -->
-
-			<div class="box">
-			<h3>Related Pages</h3>
-			 <ul><li>
-			  <a href="/plants/diversity.html"
-			   title="How many plants.">Chicago plant diversity</a>
-			 </li><li>
-			  <a href="/topics/" 
-			   title="Collections, habitats, and more.">More information on plants</a>
-			 </li><li>
-			  <a href="/resources/plant_checklist.html" 
-			   title="vPlants document.">Scientific Name Checklist</a>
-			 </li><li>
-			  <a href="/resources/links2.html"
-			   title="Links to related web sites">Links for Plants</a>
-			 </li></ul>
-			</div>
-			 
-			<p>Non-flowering vascular plants</p>
-			<img src="<?php echo $clientRoot; ?>/images.vplants/feature/nonflowering.jpg" width="250" height="342" alt="Non-flowering plant examples: fern frond, a pteridophyte; and spruce branch, a gymnosperm.">
-			<hr>
-			<p>Flowering plants </p>
-			<img src="<?php echo $clientRoot; ?>/images.vplants/feature/angios.jpg" width="250" height="322" alt="Flowering plant examples: flowers of cactus and wild rice, both angiosperms.">
-			 
-			 
-			<p class="small">Information provided on this page applies to the Chicago Region and may not be relevant or complete for other regions.</p><p class="small noprint"><a href="/disclaimer.html" title="Read Disclaimer.">Disclaimer</a></p>
-
-		</div><!-- end of #content2 -->
+					</div><!-- end of #content2 -->
+				</div><!-- end of #wrapper1 -->
+			</div><!-- end of #bodywrap -->
+		</div><!-- end of #innertext -->
 
 	<?php
 	include($serverRoot."/footer.php");
