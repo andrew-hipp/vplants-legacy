828bf458a94d44ddd72d24eb18e068f9980a50b4
kelueders@gmail.com <kelueders@gmail.com@777f1ae2-0b62-46ad-408f-69f39a835e02>
2015-01-30 00:11:39 +0000

    fixed positioning

--- trunk/symbiota.new/vPlantsNEW/plants/origin2.php
+++ trunk/symbiota.new/vPlantsNEW/plants/origin2.php
@@ -19,29 +19,44 @@ header("Content-Type: text/html; charset=".$charset);
 	include($serverRoot."/header.php");
 	?> 
         <!-- This is inner text! -->
-        <div  id="innertext">
-            <h1>Non-native Plants</h1>
+		<!-- start of inner text and right side content -->
+		<div  id="innertext">
+			<div id="bodywrap">
+				<div id="wrapper1"><!-- for navigation and content -->
 
-            <div style="margin:20px;">
-            	<p>Non-native species (aliens) are not native to an area, but rather were introduced by humans, either intentionally or accidentally. These introductions can be very dangerous to natural occurring plant communities because the non-native plants can negatively modify the habitat and thus decrease the survival of native species.  In many ways they out-compete the native species because they lack natural inhibitors (such as disease or predators).  While many alien species of plants do not make changes to the natural environment or are not able to survive outside of cultivation, many non-native plant species are problematic because they can become <a href="origin3.php">invasive</a>.
+					<!-- PAGE CONTENT STARTS -->
 
-				<dl><dt>Escaped species</dt><dd> 
-				are those that were purposely introduced to an area for agriculture, ornamental gardening, or for land stabilization (i.e. along steep slopes, riverbanks, or floodplains), but spread beyond cultivation by reproducing on their own.  Many times these plants are only localized to disturbed areas and cannot penetrate native habitats.  However, many aggressive alien plants that are now invasive were first introduced and later escaped.</dd>
-				<dt>Weeds or weedy species</dt><dd> 
-				are difficult to define because the term weed is subjective.  A particular plant is characterized as a weed only because it is growing in a location that a human does not want it to grow.  Perhaps a more appropriate term is prolific species, those that reproduce in large numbers and can spread widely.  There are both native and non-native plants that can be categorized under the terms weedy or prolific.</dd></dl></p>
-            </div>
-        </div>
+					<div id="content1wrap"><!--  for content1 only -->
+
+					<div id="content1"><!-- start of primary content --><a id="pagecontent" name="pagecontent"></a>
+					
+						<h1>Non-native Plants</h1>
+
+						<div style="margin:20px;">
+							<p>Non-native species (aliens) are not native to an area, but rather were introduced by humans, either intentionally or accidentally. These introductions can be very dangerous to natural occurring plant communities because the non-native plants can negatively modify the habitat and thus decrease the survival of native species.  In many ways they out-compete the native species because they lack natural inhibitors (such as disease or predators).  While many alien species of plants do not make changes to the natural environment or are not able to survive outside of cultivation, many non-native plant species are problematic because they can become <a href="origin3.php">invasive</a>.
+
+							<dl><dt>Escaped species</dt><dd> 
+							are those that were purposely introduced to an area for agriculture, ornamental gardening, or for land stabilization (i.e. along steep slopes, riverbanks, or floodplains), but spread beyond cultivation by reproducing on their own.  Many times these plants are only localized to disturbed areas and cannot penetrate native habitats.  However, many aggressive alien plants that are now invasive were first introduced and later escaped.</dd>
+							<dt>Weeds or weedy species</dt><dd> 
+							are difficult to define because the term weed is subjective.  A particular plant is characterized as a weed only because it is growing in a location that a human does not want it to grow.  Perhaps a more appropriate term is prolific species, those that reproduce in large numbers and can spread widely.  There are both native and non-native plants that can be categorized under the terms weedy or prolific.</dd></dl></p>
+						</div>
+					</div><!-- end of #content1 -->
+					</div><!-- end of #content1wrap -->
+        
 		
-		<div id="content2">
+					<div id="content2">
 
-			<img src="<?php echo $clientRoot; ?>/images/vplants/feature/ABTH.jpg" width="250" height="293" alt="photo of agricultural weed" title="Abutilon theophrasti">
-			<div class="box imgtext">
-			<p>Velvet leaf, <a href="../plants/species/species.jsp?gid=138"><i>Abutilon theophrasti</i></a>, is a native to Asia and now a common weed of cultivated ground.</p>
-			</div>
+						<img src="<?php echo $clientRoot; ?>/images/vplants/feature/ABTH.jpg" width="250" height="293" alt="photo of agricultural weed" title="Abutilon theophrasti">
+						<div class="box imgtext">
+						<p>Velvet leaf, <a href="../plants/species/species.jsp?gid=138"><i>Abutilon theophrasti</i></a>, is a native to Asia and now a common weed of cultivated ground.</p>
+						</div>
 
-			<p class="small">Information provided on this page applies to the Chicago Region and may not be relevant or complete for other regions.</p><p class="small noprint"><a href="../disclaimer.php" title="Read Disclaimer.">Disclaimer</a></p>
+						<p class="small">Information provided on this page applies to the Chicago Region and may not be relevant or complete for other regions.</p><p class="small noprint"><a href="../disclaimer.php" title="Read Disclaimer.">Disclaimer</a></p>
 
-		</div><!-- end of #content2 -->
+					</div><!-- end of #content2 -->
+				</div><!-- end of #wrapper1 -->
+			</div><!-- end of #bodywrap -->
+		</div><!-- end of #innertext -->
 
 	<?php
 	include($serverRoot."/footer.php");
--- trunk/symbiota.new/vPlantsNEW/plants/origin4.php
+++ trunk/symbiota.new/vPlantsNEW/plants/origin4.php
@@ -19,24 +19,37 @@ header("Content-Type: text/html; charset=".$charset);
 	include($serverRoot."/header.php");
 	?> 
         <!-- This is inner text! -->
-        <div  id="innertext">
-            <h1>Native Plants</h1>
+		<!-- start of inner text and right side content -->
+		<div  id="innertext">
+			<div id="bodywrap">
+				<div id="wrapper1"><!-- for navigation and content -->
 
-            <div style="margin:20px;">
-            	<p>Native plants are adapted to this region and optimized for the specific conditions of the area.  They succeed in areas where the natural habitat is still intact.  While it is sometimes straightforward to classify a particular plant as being native or non-native, it can often be a challenge to decide on the "natural" distribution of a species.  For this project, with few exceptions, we have agreed with the designations made by Swink and Wilhelm (1994).  The assigned status of a particular taxon is indicated on the Description pages under the section "Regional occurrence."  See <a href="../plants/species/species.jsp?gid=6813" title="description page"><i>Campanula americana</i></a> for an example of a native plant species.</p>
-            </div>
-        </div>
+					<!-- PAGE CONTENT STARTS -->
+
+					<div id="content1wrap"><!--  for content1 only -->
+
+					<div id="content1"><!-- start of primary content --><a id="pagecontent" name="pagecontent"></a>
+						<h1>Native Plants</h1>
+
+						<div style="margin:20px;">
+							<p>Native plants are adapted to this region and optimized for the specific conditions of the area.  They succeed in areas where the natural habitat is still intact.  While it is sometimes straightforward to classify a particular plant as being native or non-native, it can often be a challenge to decide on the "natural" distribution of a species.  For this project, with few exceptions, we have agreed with the designations made by Swink and Wilhelm (1994).  The assigned status of a particular taxon is indicated on the Description pages under the section "Regional occurrence."  See <a href="../plants/species/species.jsp?gid=6813" title="description page"><i>Campanula americana</i></a> for an example of a native plant species.</p>
+						</div>
+					</div><!-- end of #content1 -->
+					</div><!-- end of #content1wrap -->
 		
-		<div id="content2">
+					<div id="content2">
 
-			<img src="<?php echo $clientRoot; ?>/images/vplants/feature/ABTH.jpg" width="250" height="293" alt="photo of agricultural weed" title="Abutilon theophrasti">
-			<div class="box imgtext">
-			<p>Velvet leaf, <a href="../plants/species/species.jsp?gid=138"><i>Abutilon theophrasti</i></a>, is a native to Asia and now a common weed of cultivated ground.</p>
-			</div>
+						<img src="<?php echo $clientRoot; ?>/images/vplants/feature/ABTH.jpg" width="250" height="293" alt="photo of agricultural weed" title="Abutilon theophrasti">
+						<div class="box imgtext">
+						<p>Velvet leaf, <a href="../plants/species/species.jsp?gid=138"><i>Abutilon theophrasti</i></a>, is a native to Asia and now a common weed of cultivated ground.</p>
+						</div>
 
-			<p class="small">Information provided on this page applies to the Chicago Region and may not be relevant or complete for other regions.</p><p class="small noprint"><a href="../disclaimer.php" title="Read Disclaimer.">Disclaimer</a></p>
+						<p class="small">Information provided on this page applies to the Chicago Region and may not be relevant or complete for other regions.</p><p class="small noprint"><a href="../disclaimer.php" title="Read Disclaimer.">Disclaimer</a></p>
 
-		</div><!-- end of #content2 -->
+					</div><!-- end of #content2 -->
+				</div><!-- end of #wrapper1 -->
+			</div><!-- end of #bodywrap -->
+		</div><!-- end of #innertext -->
 
 	<?php
 	include($serverRoot."/footer.php");
