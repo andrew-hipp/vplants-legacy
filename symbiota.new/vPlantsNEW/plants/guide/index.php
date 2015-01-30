0f5058187d040d3e8f47e0614cabb2d6f15673f1
kelueders@gmail.com <kelueders@gmail.com@777f1ae2-0b62-46ad-408f-69f39a835e02>
2015-01-30 17:55:28 +0000

    fixed styling

--- symbiota.new/vPlantsNEW/plants/guide/index.php
+++ symbiota.new/vPlantsNEW/plants/guide/index.php
@@ -1,13 +1,13 @@
 <?php
 //error_reporting(E_ALL);
-include_once('../config/symbini.php');
+include_once('../../config/symbini.php');
 header("Content-Type: text/html; charset=".$charset);
 ?>
 <html>
 <head>
 	<title><?php echo $defaultTitle?>vPlants - Guide to Plants</title>
-	<link href="../css/base.css" type="text/css" rel="stylesheet" />
-	<link href="../css/main.css" type="text/css" rel="stylesheet" />
+	<link href="../../css/base.css" type="text/css" rel="stylesheet" />
+	<link href="../../css/main.css" type="text/css" rel="stylesheet" />
 	<meta name='keywords' content='' />
 	<script type="text/javascript">
 		<?php include_once($serverRoot.'/config/googleanalytics.php'); ?>
