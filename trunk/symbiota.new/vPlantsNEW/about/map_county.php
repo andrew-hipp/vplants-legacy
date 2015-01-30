e38e9643fed1b88c31971b24e017f921fb51ae02
kelueders@gmail.com <kelueders@gmail.com@777f1ae2-0b62-46ad-408f-69f39a835e02>
2015-01-30 16:00:48 +0000

    fixed positioning and added submenu

--- trunk/symbiota.new/vPlantsNEW/about/map_county.php
+++ trunk/symbiota.new/vPlantsNEW/about/map_county.php
@@ -18,97 +18,82 @@ header("Content-Type: text/html; charset=".$charset);
 	$displayLeftMenu = "true";
 	include($serverRoot."/header.php");
 	?> 
-		<!-- Left menu begins -->
-		<div class="menu">
-			<div class="menuheader">
-				<a href="<?php echo $clientRoot; ?>/about/index.php">
-					About Us
-				</a>
-			</div>
-			<div class="menuitem">
-				<a href="<?php echo $clientRoot; ?>/about/vplants.php">
-					vPlants Project
-				</a>
-			</div>
-			<div class="menuitem">
-				<a href="<?php echo $clientRoot; ?>/about/chicago.php">
-				Chicago Region
-				</a>
-			</div>
-				<div class="submenuitem">
-					<a href="<?php echo $clientRoot; ?>/about/map.php">State Map</a>
-				</div>
-				<div class="submenuitem">
-					<strong>County Map</strong>
-				</div>
-			<div class="menuitem">
-				<a href="<?php echo $clientRoot; ?>/about/partnership.php">
-					vPlants Partnership
-				</a>
-			</div>
-			<div class="menuitem">
-				<a href="<?php echo $clientRoot; ?>/about/contact.php">
-					Contact Us
-				</a>
-			</div>
-			<div class="menuitem">
-				<a href="<?php echo $clientRoot; ?>/about/credits.php">
-					Credits
-				</a>
-			</div>
-		</div>
-		<!-- Left menu ends -->
-		
         <!-- This is inner text! -->
-        <div  id="innertext">
-            <h1>County Map for Chicago Region</h1>
+		<!-- start of inner text and right side content -->
+		<div  id="innertext">
+			<div id="bodywrap">
+				<div id="wrapper1"><!-- for navigation and content -->
 
-            <div style="margin:20px;">
-            	 <p>Map of the 24 counties included in the Chicago Region for vPlants. Please be aware when searching records that there is a Lake County in Illinois as well as a Lake County in Indiana.</p>
- 
-				 <img class="floatleft" src="<?php echo $clientRoot; ?>/images/vplants/img/map_vplants.gif" width="490" height="484" alt="Map of the vPlants Chicago Region showing counties included">
+					<!-- PAGE CONTENT STARTS -->
+
+					<div id="content1wrap"><!--  for content1 only -->
+
+					<div id="content1"><!-- start of primary content --><a id="pagecontent" name="pagecontent"></a>
+						<h1>County Map for Chicago Region</h1>
+
+						<div style="margin:20px;">
+							 <p>Map of the 24 counties included in the Chicago Region for vPlants. Please be aware when searching records that there is a Lake County in Illinois as well as a Lake County in Indiana.</p>
+			 
+							 <img class="floatleft" src="<?php echo $clientRoot; ?>/images/vplants/img/map_vplants.gif" width="490" height="484" alt="Map of the vPlants Chicago Region showing counties included">
 
 
-				 <div class="floatleft">
-				  <h3>Illinois</h3>
-				  <ul>
-				   <li>Boone</li>
-				   <li>Cook</li>
-				   <li>DeKalb</li>
-				   <li>DuPage</li>
-				   <li>Grundy</li>
-				   <li>Kane</li>
-				   <li>Kankakee</li>
-				   <li>Kendall</li>
-				   <li>Lake</li>
-				   <li>McHenry</li>
-				   <li>Will</li>
-				  </ul>
-				  <h3>Indiana</h3>
-				  <ul>
-				   <li>Jasper</li>
-				   <li>Lake</li>
-				   <li>LaPorte</li>
-				   <li>Newton</li>
-				   <li>Porter</li>
-				   <li>Starke</li>
-				   <li>St. Joseph</li>
-				  </ul>
-				  <h3>Michigan</h3>
-				  <ul>
-				   <li>Berrien</li>
-				  </ul>
-				  <h3>Wisconsin</h3>
-				  <ul>
-				   <li>Kenosha</li>
-				   <li>Milwaukee</li>
-				   <li>Racine</li>
-				   <li>Walworth</li>
-				   <li>Waukesha</li>
-				  </ul>
-				 </div>
-            </div>
-        </div>
+							<div class="floatleft">
+								  <h3>Illinois</h3>
+								  <ul>
+								   <li>Boone</li>
+								   <li>Cook</li>
+								   <li>DeKalb</li>
+								   <li>DuPage</li>
+								   <li>Grundy</li>
+								   <li>Kane</li>
+								   <li>Kankakee</li>
+								   <li>Kendall</li>
+								   <li>Lake</li>
+								   <li>McHenry</li>
+								   <li>Will</li>
+								  </ul>
+								  <h3>Indiana</h3>
+								  <ul>
+								   <li>Jasper</li>
+								   <li>Lake</li>
+								   <li>LaPorte</li>
+								   <li>Newton</li>
+								   <li>Porter</li>
+								   <li>Starke</li>
+								   <li>St. Joseph</li>
+								  </ul>
+								  <h3>Michigan</h3>
+								  <ul>
+								   <li>Berrien</li>
+								  </ul>
+								  <h3>Wisconsin</h3>
+								  <ul>
+								   <li>Kenosha</li>
+								   <li>Milwaukee</li>
+								   <li>Racine</li>
+								   <li>Walworth</li>
+								   <li>Waukesha</li>
+								  </ul>
+							</div>
+						</div>
+					</div><!-- end of #content1 -->
+					</div><!-- end of #content1wrap -->
+					
+					<div id="content2">
+								
+						<div class="box">
+							<h3>Chicago Region Maps</h3>
+							<ul>
+								<li><a href="chicago.php">Chicago Region Main</a></li>
+								<li><a href="map.php">State Map</a></li>
+								<li><strong>County Map</strong></li>
+							</ul>
+						</div>
+									
+					</div><!-- end of #content2 -->
+				</div><!-- end of #wrapper1 -->
+			</div><!-- end of #bodywrap -->
+		</div><!-- end of #innertext -->
 
 	<?php
 	include($serverRoot."/footer.php");
