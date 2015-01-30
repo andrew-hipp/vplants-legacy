7c22efb2bcc137b4228d2c5c672ec83567a57ddb
kelueders@gmail.com <kelueders@gmail.com@777f1ae2-0b62-46ad-408f-69f39a835e02>
2015-01-30 17:45:53 +0000

    fixed positioning, added right side menu, changed formatting of main names page

--- symbiota.new/vPlantsNEW/plants/names3.php
+++ symbiota.new/vPlantsNEW/plants/names3.php
@@ -19,35 +19,52 @@ header("Content-Type: text/html; charset=".$charset);
 	include($serverRoot."/header.php");
 	?> 
         <!-- This is inner text! -->
-        <div  id="innertext">
-            <h1>Synonyms</h1>
+		<!-- start of inner text and right side content -->
+		<div  id="innertext">
+			<div id="bodywrap">
+				<div id="wrapper1"><!-- for navigation and content -->
 
-            <div style="margin:20px;">
-            	<p>Synonyms are different scientific names that have been assigned to the same organism. For example, the names <i>Aster azureus</i> and <i>Aster oolentangiensis</i> refer to the same species of aster.  Synonyms can exist when scientists have different opinions about how a specific organism should be defined, or someone names the same organism with a new name because they were unaware of the previous name.</p>
-            </div>
-        </div>
+					<!-- PAGE CONTENT STARTS -->
+
+					<div id="content1wrap"><!--  for content1 only -->
+
+					<div id="content1"><!-- start of primary content --><a id="pagecontent" name="pagecontent"></a>
+						<h1>Synonyms</h1>
+
+						<div style="margin:20px;">
+							<p>Synonyms are different scientific names that have been assigned to the same organism. For example, the names <i>Aster azureus</i> and <i>Aster oolentangiensis</i> refer to the same species of aster.  Synonyms can exist when scientists have different opinions about how a specific organism should be defined, or someone names the same organism with a new name because they were unaware of the previous name.</p>
+						</div>
+					</div><!-- end of #content1 -->
+					</div><!-- end of #content1wrap -->
 		
-		<div id="content2">
+					<div id="content2">
 
-			<div class="box document">
-			<h3>....</h3>
-			<ul><li>
-			....
-			</li></ul>
-			</div>
+						<div class="box">
+							<h3>Names</h3>
+							<ul>
+								<li><a href="names.php">Names Main</a></li>
+								<li><a href="names5.php">Taxonomy</a></li>
+								<li><a href="names2.php">Scientific names</a></li>
+								<li><strong>Synonyms</strong></li>
+								<li><a href="names4.php">Common names</a></li>
+							</ul>
+						</div>
 
-			<div class="box external">
-			<h3>....</h3>
-			<ul>
-			<li>
-			<!-- link to Index Fungorum -->
-			</li>
-			</ul>
-			</div>
+						<div class="box external">
+						<h3>....</h3>
+						<ul>
+						<li>
+						<!-- link to Index Fungorum -->
+						</li>
+						</ul>
+						</div>
 
-			<p class="small">Information provided on this page applies to the Chicago Region and may not be relevant or complete for other regions.</p><p class="small noprint"><a href="../disclaimer.php" title="Read Disclaimer.">Disclaimer</a></p>
+						<p class="small">Information provided on this page applies to the Chicago Region and may not be relevant or complete for other regions.</p><p class="small noprint"><a href="../disclaimer.php" title="Read Disclaimer.">Disclaimer</a></p>
 
-		</div><!-- end of #content2 -->
+					</div><!-- end of #content2 -->
+				</div><!-- end of #wrapper1 -->
+			</div><!-- end of #bodywrap -->
+		</div><!-- end of #innertext -->
 
 	<?php
 	include($serverRoot."/footer.php");
