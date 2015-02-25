106ad6cc3084f397fe1a39a773acd2c7aa00bf38
ahipp@mortonarb.org <ahipp@mortonarb.org@777f1ae2-0b62-46ad-408f-69f39a835e02>
2015-02-25 14:28:44 +0000

    adding hyperlinks to vplants.php

--- symbiota.new/vPlantsNEW/about/vplants.php
+++ symbiota.new/vPlantsNEW/about/vplants.php
@@ -30,15 +30,15 @@ header("Content-Type: text/html; charset=".$charset);
 					<div id="content1"><!-- start of primary content --><a id="pagecontent" name="pagecontent"></a>
 					<h1>The vPlants Project</h1>
 					<div style="margin:20px;">
-            	<p>The Morton Arboretum, the Field Museum of Natural History, and the Chicago Botanic Garden developed vPlants (“virtual Plants”) as an online, searchable database to provide free web access to data and digital images of plant specimens collected in the Chicago Region. The project began in January 2001 and was initially funded by the Institute of Museum and Library Services. Chicago Wilderness and the Newman Family Fund also provided support for the project.
+            	<p><a href="http://www.mortonarb.org/">The Morton Arboretum</a>, the <a href="http://www.fieldmuseum.org/">Field Museum of Natural History</a>, and the <a href="http://www.chicagobotanic.org/">Chicago Botanic Garden</a> developed vPlants (“virtual Plants”) as an online, searchable database to provide free web access to data and digital images of plant specimens collected in the Chicago Region. The project began in January 2001 and was initially funded by the <a href="http://www.imls.gov/">Institute of Museum and Library Services</a>. <a href="http://www.chicagowilderness.org/">Chicago Wilderness</a> and the Newman Family Fund also provided support for the project.
 				</p>
 				<p>vPlants was built using XML-based software that allowed these three founding institutions to pool data from their disparate hardware and database systems. Information housed at each institution was transferred, using XML, into a single web-searchable database at the vPlants portal. At the time it was a nifty system.
 				</p>
 				<p>In the ensuing years, some of the elements of the vPlants system did not age well. Although the XML-based elements continued to provide very fast database searches, some of the software pieces that connected the system to the web could not be upgraded. This frustrated our users, who found our search engine increasingly unresponsive over time.
 				</p>
-				<p>In 2015, we moved the vPlants data and static pages to the well tested Symbiota platform  (Gries et al. 2014).
+				<p>In 2015, we moved the vPlants data and static pages to the well tested <a href="http://www.symbiota.org/">Symbiota</a> platform  (<a href="http://www.ncbi.nlm.nih.gov/pubmed/25057252">Gries et al. 2014</a>).
 				</p>
-				<p>The original vPlants data pool allowed users to search data from 80,000 plant specimens housed in the herbaria of each of the three founding institutions. The data pool also contained digital images for almost 50,000 of those specimens. By associating ourselves with the much larger network of herbaria that use Symbiota and contribute data via SEINet, users of our new and improved vPlants web portal can search data from over 120,000 plant specimens and have access to new tools.
+				<p>The original vPlants data pool allowed users to search data from 80,000 plant specimens housed in the herbaria of each of the three founding institutions. The data pool also contained digital images for almost 50,000 of those specimens. By associating ourselves with the much larger network of herbaria that use Symbiota and contribute data via <a href="http://swbiodiversity.org/seinet/projects/index.php">SEINet</a>, users of our new and improved vPlants web portal can search data from over 120,000 plant specimens and have access to new tools.
 				</p>
 				<p>We hope that our participation in this Symbiota-based network is a significant step towards building a larger online portal for plants found throughout the Western Great Lakes Region.
 				</p>
